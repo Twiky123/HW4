0b4d249d6cb74a66e83383417b2b0bfd994f5dc6
Oleg <tvikster4@gmail.com>
2023-10-05 18:49:23 +0300

    \\lp

---
+++ dz4_/dz4_/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dz4_
+{
+    internal class Program
+    {
+        static void ChangesValue(int firstnum, int secondnum, int[] random)
+        {
+            int firstIndex = Array.IndexOf(random, firstnum);
+            int secondIndex = Array.IndexOf(random, secondnum);
+
+            random[firstIndex] = secondnum;
+            random[secondIndex] = firstnum;
+        }
+        static int Calculationsnums(ref int product, out double averageval, params int[] randomvals)
+        {
+            int summ = 0;
+
+            foreach (int number in randomvals)
+            {
+                summ += number;
+                product *= number;
+            }
+
+            averageval = (double)summ / randomvals.Length;
+
+            return summ;
+        }
+        static void Draw(string[] Numpikctu)
+        {
+            try
+            {
+                string srtinge;
+                int num;
+                bool result;
+
+                do
+                {
+                    Console.Write("Введите целое число от 0 до 9: ");
+                    srtinge = Console.ReadLine();
+                    result = int.TryParse(srtinge, out num);
+
+                    if (result)
+                    {
+                        if (num >= 0 && num <= 9)
+                        {
+                            Console.WriteLine(Numpikctu[num]);
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Вы ввели число меньше 0 или больше 9");
+                            Thread.Sleep(3000);
+                            Console.ResetColor();
+                        }
+                    }
+                    else if (srtinge.ToLower() == "exit" || srtinge.ToLower() == "закрыть")
+                    {
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        throw new Exception("Вы ввели не число или оно является дробным");
+                    }
+                } while (!(srtinge.ToLower() == "exit" || srtinge.ToLower() == "закрыть"));
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                Console.Write("Чтобы закончить выполнение упражнения, нажмите на любую кнопку ");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 1");
+            int[] random = new int[20];
+            int firstnum, secondnum;
+            bool firstres, secondres;
+            Random random1 = new Random();
+            Console.WriteLine();
+            for (int i = 0; i < random.Length; i++)
+            {
+                int number = random1.Next(50);
+                Console.Write(number + " ");
+                random[i] = number;
+            }
+            Console.WriteLine("Два числа из массива меняются местами");
+
+            do
+            {
+                Console.Write("Введите первое число из массива: ");
+                firstres = int.TryParse(Console.ReadLine(), out firstnum);
+                Console.Write("Введите второе число из массива: ");
+                secondres = int.TryParse(Console.ReadLine(), out secondnum);
+
+                if (firstres && secondres && (Array.IndexOf(random, firstnum) != -1) && (Array.IndexOf(random, secondnum) != -1))
+                {
+                    ChangesValue(firstnum, secondnum, random);
+
+                    Console.Write("Массив изменился: ");
+
+                    foreach (int number in random)
+                    {
+                        Console.Write(number + " ");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели неверные данные");
+                }
+            } while (!(firstres && secondres && (Array.IndexOf(random, firstnum) != -1) && (Array.IndexOf(random, secondnum) != -1)));
+
+            Console.WriteLine("Упражнение 2");
+
+            Random randomval = new Random();
+            int[] randomvals = new int[randomval.Next(1, 15)];
+            int summ;
+            double averageval;
+            int product = 1;
+
+            Console.Write("Получился массив: ");
+
+            for (int i = 0; i < randomvals.Length; i++)
+            {
+                int number = randomval.Next(20);
+                Console.Write(number + " ");
+                randomvals[i] = number;
+            }
+
+            summ = Calculationsnums(ref product, out averageval, randomvals);
+
+            Console.WriteLine($"Сумма чисел равна {summ}, произведение - {product}, среднее арифметическое - {averageval:F}");
+
+            Console.WriteLine();
+
+            string[] Numpikctu = { "\t#####\n\t#   #\n\t#   #\n\t#   #\n\t#   #\n\t#   #\n\t#####\n",
+                                                     "\t    #\n\t  # #\n\t #  #\n\t    #\n\t    #\n\t    #\n\t    #\n",
+                                                     "\t#####\n\t    #\n\t    #\n\t#####\n\t#    \n\t#    \n\t#####\n",
+                                                     "\t#####\n\t    #\n\t    #\n\t#####\n\t    #\n\t    #\n\t#####\n",
+                                                     "\t#   #\n\t#   #\n\t#   #\n\t#####\n\t    #\n\t    #\n\t    #\n",
+                                                     "\t#####\n\t#    \n\t#    \n\t#####\n\t    #\n\t    #\n\t#####\n",
+                                                     "\t#####\n\t#    \n\t#    \n\t#####\n\t#   #\n\t#   #\n\t#####\n",
+                                                     "\t######\n\t#    #\n\t    #\n\t  #####\n\t    #\n\t    #\n\t    #\n",
+                                                     "\t#####\n\t#   #\n\t#   #\n\t#####\n\t#   #\n\t#   #\n\t#####\n",
+                                                     "\t#####\n\t#   #\n\t#   #\n\t#####\n\t    #\n\t    #\n\t#####\n" };
+            Draw(Numpikctu);
+
+
+        }
+    }
+}
---
+++ labatymalova5/labatymalova5/Program.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labatymalova5
+{
+    class Program
+    {
+        static int Maxnum(int firstnum, int secondnum)
+        {
+            if (firstnum > secondnum)
+            {
+                return firstnum;
+            }
+            else
+            {
+                return secondnum;
+            }
+        }
+        static void Changevalue(ref string firstvalue, ref string secondvalue)
+        {
+            string thirdvalue = firstvalue;
+            firstvalue = secondvalue;
+            secondvalue = thirdvalue;
+        }
+        static bool Factorial(out int factorial, int num)
+        {
+            try
+            {
+                factorial = 1;
+
+                for (int i = 1; i <= num; i++)
+                {
+                    checked
+                    {
+                        factorial *= i;
+                    }
+                }
+                return true;
+            }
+            catch (OverflowException)
+            {
+                factorial = -1;
+                return false;
+            }
+        }
+        static int Factorialrecursion(int num1)
+        {
+            if (num1 == 1)
+            {
+                return 1;
+            }
+            return num1 * Factorialrecursion(num1 - 1);
+        }
+        static int NOD(int firstnum2, int secondnum2)
+        {
+            int ost, result;
+            do
+            {
+                ost = firstnum2 % secondnum2;
+                firstnum2 = secondnum2;
+                result = secondnum2;
+                secondnum2 = ost;
+            } while (ost != 0);
+
+            return result;
+        }
+        static int NOD(int firstnum3, int secondnum3, int thirdnum3)
+        {
+            int del = NOD(firstnum3, secondnum3);
+            int result = NOD(del, thirdnum3);
+
+            return result;
+        }
+        static int Fibonnum(int numberval)
+        {
+            if (numberval == 1 || numberval == 2)
+            {
+                return 1;
+            }
+
+            return Fibonnum(numberval - 1) + Fibonnum(numberval - 2);
+        }
+        static void Main(string[] args)
+        {
+            bool taskend = false;
+            string tasknum;
+            do
+            {
+                Console.WriteLine("Лаба Тумакова 5 - меню заданий");
+                Console.WriteLine("Подсказка:\n" +
+                                  "Упражнение 5.1. Программа возвращает максимальное из двух введенных целых чисел   -   цифра 1\n" +
+                                  "Упражнение 5.2. Программа меняет местами два передаваемых значения                -   цифра 2\n" +
+                                  "Упражнение 5.3. Программа вычисляет факториал числа с помощью цикла               -   цифра 3\n" +
+                                  "Упражнение 5.4. Программа вычисляет факториал числа с помощью рекурсии            -   цифра 4\n" +
+                                  "Домашнее задание 5.1. Программа вычисляет НОД двух и трех чисел                   -   цифра 5\n" +
+                                  "Домашнее задание 5.2. Программа вычисляет значение n-го числа ряда Фибоначчи      -   цифра 6\n" +
+                                  "Выйти из программы                                                                -   цифра 0\n");
+
+                Console.Write("Введите номер задания: ");
+                tasknum = Console.ReadLine();
+                
+
+                switch (tasknum)
+                {
+                    case "1":
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Упражнение 5.1");
+
+                            int firstnum, secondnum, Maxnum1;
+                            bool firstres, secondres;
+
+                            do
+                            {
+                                Console.Write("Введите первое целое число: ");
+                                firstres = int.TryParse(Console.ReadLine(), out firstnum);
+                                Console.Write("Введите второе целое число: ");
+                                secondres = int.TryParse(Console.ReadLine(), out secondnum);
+
+                                if (firstres && secondres)
+                                {
+                                    Maxnum1 = Maxnum(firstnum, secondnum);
+
+                                    Console.WriteLine($"max({firstnum}, {secondnum}) = {Maxnum1}");
+                                    Console.Write("Чтобы закончить выполнение упражнения, нажмите на любую кнопку ");
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Вы ввели неверные данные, повторите попытку");
+                                }
+                            } while (!(firstres && secondres));
+                            break;
+                        }
+                    case "2":
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Упражнение 5.2");
+                            string firstvalue, secondvalue;
+
+                            Console.Write("Введите первое значение: ");
+                            firstvalue = Console.ReadLine();
+                            Console.Write("Введите второе значение: ");
+                            secondvalue = Console.ReadLine();
+
+                            Changevalue(ref firstvalue, ref secondvalue);
+
+                            Console.WriteLine($"Значения поменялись местами \n Первое значение - {firstvalue}, второе значение - {secondvalue}");
+                            Console.Write("Нажмите на любую кнопку ");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        }
+                    case "3":
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Упражнение 5.3");
+                            int factorial, num;
+                            bool factorialres = true;
+                            bool parseres;
+
+                            do
+                            {
+                                Console.Write("Введите натуральное число, большее нуля: ");
+                                parseres = int.TryParse(Console.ReadLine(), out num);
+
+                                if (parseres && num > 0)
+                                {
+                                    factorialres = Factorial(out factorial, num);
+
+                                    if (factorialres)
+                                    {
+                                        Console.WriteLine($"{num}! = {factorial}");
+                                        Console.Write("Чтобы закончить выполнение упражнения, нажмите на любую кнопку ");
+                                        Console.ReadKey();
+                                        Console.Clear();
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine($"При вычислении {num} получилось слишком большое число");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Вы ввели число или оно не натуральное");
+                                }
+                            } while (!(parseres && num > 0 && factorialres));
+                            break;
+                        }
+                    case "4":
+                        Console.Clear();
+                        Console.WriteLine("Упражнение 5.4");
+                        int factorial1, num1;
+                        bool parseres1;
+
+                        do
+                        {
+                            Console.Write("Введите натуральное число, большее нуля: ");
+                            parseres1 = int.TryParse(Console.ReadLine(), out num1);
+
+                            if (parseres1 && num1 > 0)
+                            {
+                                factorial1 = Factorialrecursion(num1);
+
+                                if (factorial1 == 0)
+                                {
+                                    Console.WriteLine($"При вычислении {num1} получилось слишком большое число");
+                                    Console.Write("Чтобы выйти нажмите на любую кнопку");
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"{num1}! = {factorial1}");
+                                    Console.Write("Чтобы выйти нажмите на любую кнопку");
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Вы Ввели неверные данные ");
+                            }
+                        } while (!(parseres1 && num1 > 0));
+                        break;
+                    case "5":
+                        Console.Clear();
+                        Console.WriteLine("Домашнее упражнение 5.1");
+
+                        int firstnum2, secondnum2, thirdnum2, nod;
+                        bool firstres2, secondres2, thirdres2;
+
+                        do
+                        {
+                            Console.Write("Введите первое натуральное число: ");
+                            firstres2 = int.TryParse(Console.ReadLine(), out firstnum2);
+                            Console.Write("Введите второе натуральное число: ");
+                            secondres2 = int.TryParse(Console.ReadLine(), out secondnum2);
+
+                            if ((firstres2 && secondres2) && (firstnum2 > 0 && secondnum2 > 0))
+                            {
+                                nod = NOD(firstnum2, secondnum2);
+
+                                Console.WriteLine($"НОД({firstnum2}, {secondnum2}) = {nod}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Вы ввели не число или оно не натуральное");
+                            }
+                        } while (!((firstres2 && secondres2) && (firstnum2 > 0 && secondnum2 > 0)));
+
+                        do
+                        {
+                            Console.Write("Введите первое натуральное число: ");
+                            firstres2 = int.TryParse(Console.ReadLine(), out firstnum2);
+                            Console.Write("Введите второе натуральное число: ");
+                            secondres2 = int.TryParse(Console.ReadLine(), out secondnum2);
+                            Console.Write("Введите третье натуральное число: ");
+                            thirdres2 = int.TryParse(Console.ReadLine(), out thirdnum2);
+
+                            if ((firstres2 && secondres2 && thirdres2) && (firstnum2 > 0 && secondnum2 > 0 && thirdnum2 > 0))
+                            {
+                                nod = NOD(firstnum2, secondnum2, thirdnum2);
+
+                                Console.WriteLine($"НОД({firstnum2}, {secondnum2}, {thirdnum2}) = {nod}");
+                                Console.Write("Чтобы завершить что-нибудь тыкните");
+                                Console.ReadKey();
+                                Console.Clear();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Число является не числом или оно не натуральное");
+                            }
+                        } while (!((firstres2 && secondres2 && thirdres2) && (firstnum2 > 0 && secondnum2 > 0 && thirdnum2 > 0)));
+                        break;
+
+                    case "6":
+                        Console.Clear();
+                        Console.WriteLine("ДЗ упражнение 5.2");
+                        int numberval, fibonnum;
+                        bool parseres3;
+                        do
+                        {
+                            Console.Write("Введите номер числа из ряда Фибоначчи: ");
+                            parseres3 = int.TryParse(Console.ReadLine(), out numberval);
+
+                            if (parseres3 && numberval > 0)
+                            {
+                                fibonnum = Fibonnum(numberval);
+
+                                Console.WriteLine($"{numberval} число из ряда Фибоначчи равно {fibonnum}");
+                                Console.Write("Чтобы закончить выполнение упражнения, нажмите на любую кнопку ");
+                                Console.ReadKey();
+                                Console.Clear();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Вы ввели неверные данные");
+                            }
+                        } while (!(parseres3 && numberval > 0));
+                        break;
+
+                    case "0":
+                        Console.Clear();
+                        Console.WriteLine("Завершение работы");
+                        taskend = true;
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Нет такого задания");
+                        break;
+                }
+            } while (!taskend);
+        }
+    }
+}
